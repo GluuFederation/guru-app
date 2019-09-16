b3d9a1a037a3cf6d8bf605ab4b5593802823ffd0
Gerald Pharin <pharingee@Fafa.local>
2019-09-16 14:45:15 +0000

    Add remote deploy script

---
+++ devops/remote-deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+ENVIRONMENT=$1
+SCRIPTDIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+if [ -z $ENVIRONMENT ]; then
+  ENVIRONMENT=dev
+fi
+
+$SCRIPTDIR/build.py guru-api --test true --push true --deploy true
+$SCRIPTDIR/build.py guru-react --test true --push true
+$SCRIPTDIR/build.py users-vue --test true --push true
+$SCRIPTDIR/build.py guru-nginx --test true --push true --deploy true --env ENVIRONMENT
